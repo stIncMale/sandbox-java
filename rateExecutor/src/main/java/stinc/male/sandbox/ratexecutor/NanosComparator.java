6178cb7070fea3e2b86167e18a8bb8820bdcf6e3
Valentin Kovalenko <valentink@zeptolab.com>
2016-04-22 21:25:45 +0300

    -

--- rateExecutor/src/main/java/stinc/male/sandbox/ratexecutor/NanosComparator.java
+++ rateExecutor/src/main/java/stinc/male/sandbox/ratexecutor/NanosComparator.java
@@ -1,18 +1,27 @@
 package stinc.male.sandbox.ratexecutor;
 
+import java.math.BigInteger;
 import java.util.Comparator;
 import javax.annotation.concurrent.ThreadSafe;
 import static stinc.male.sandbox.ratexecutor.Preconditions.checkNotNull;
 
 @ThreadSafe
 final class NanosComparator implements Comparator<Long> {
-	private static final NanosComparator instance = new NanosComparator();
+	private static final long MAX_DIFF = BigInteger.valueOf(2).pow(63).subtract(BigInteger.ONE).longValueExact();
+	private static final NanosComparator unsafeInstance = new NanosComparator(false);
+	private static final NanosComparator instance = new NanosComparator(true);
 
-	static final NanosComparator getInstance() {
+	public static final NanosComparator getInstance() {
 		return instance;
 	}
 
-	static final int compare(long l1, long l2) {
+	public static final NanosComparator getUnsafeInstance() {
+		return unsafeInstance;
+	}
+
+	private final boolean safe;
+
+	public static final int compareUnsafe(final long l1, final long l2) {
 		final long diff = l1 - l2;
 		final int result;
 		if (diff < 0) {
@@ -25,13 +34,41 @@ final class NanosComparator implements Comparator<Long> {
 		return result;
 	}
 
-	private NanosComparator() {
+	public static final int compare(final long l1, final long l2) throws IllegalArgumentException {
+		checkArguments(l1, l2);
+		return compareUnsafe(l1, l2);
 	}
 
 	@Override
 	public final int compare(final Long o1, final Long o2) {
 		checkNotNull(o1, "o1");
 		checkNotNull(o2, "o2");
-		return compare(o1.longValue(), o2.longValue());
+		return safe
+				? compare(o1.longValue(), o2.longValue())
+				: compareUnsafe(o1.longValue(), o2.longValue());
 	}
+
+	@Override
+	public final String toString() {
+		return getClass().getSimpleName() + "(safe=" + safe + ')';
+	}
+
+	private static final void checkArguments(final long l1, final long l2) throws IllegalArgumentException {
+		final long diff12 = l1 - l2;
+		if (Math.abs(diff12) > MAX_DIFF || diff12 == Long.MIN_VALUE) {//Math.abs can't handle Long.MIN_VALUE
+			throw new IllegalArgumentException(
+					String.format("Difference between %s and %s has greater absolute value than %s", l1, l2, MAX_DIFF));
+		}
+		final long diff21 = l2 - l1;
+		if (Math.abs(diff21) > MAX_DIFF || diff21 == Long.MIN_VALUE) {//Math.abs can't handle Long.MIN_VALUE
+			throw new IllegalArgumentException(
+					String.format("Difference between %s and %s has greater absolute value than %s", l2, l1, MAX_DIFF));
+		}
+	}
+
+	private NanosComparator(final boolean safe) {
+		this.safe = safe;
+	}
+	//VAKOTODO hashCode and equals
+	//VAKOTODO only check for diff == Long.MIN_VALUE because all other values are comparable
 }
--- rateExecutor/src/test/java/stinc/male/sandbox/ratexecutor/NanosComparatorTest.java
+++ rateExecutor/src/test/java/stinc/male/sandbox/ratexecutor/NanosComparatorTest.java
@@ -12,6 +12,8 @@ public final class NanosComparatorTest {
 			new Sample(Long.MAX_VALUE, Long.MAX_VALUE, CompareResult.EQUAL),
 			new Sample(0, 0, CompareResult.EQUAL),
 			new Sample(0, Long.MAX_VALUE, CompareResult.LOWER),//maxDelta
+			new Sample(-2, Long.MAX_VALUE, CompareResult.GREATER),//maxDelta + 1
+			new Sample(0, Long.MAX_VALUE, CompareResult.LOWER),//maxDelta
 			new Sample(-1, Long.MIN_VALUE, CompareResult.GREATER),//maxDelta
 			new Sample(Long.MAX_VALUE - Long.MAX_VALUE / 2, Long.MIN_VALUE + Long.MAX_VALUE / 2, CompareResult.LOWER),//maxDelta
 			new Sample(Long.MIN_VALUE, Long.MAX_VALUE, CompareResult.GREATER),
@@ -20,41 +22,64 @@ public final class NanosComparatorTest {
 			new Sample(5, 3, CompareResult.GREATER),
 	};
 
+	private static final Sample[] incompatibleSamples = {
+			new Sample(-1, Long.MAX_VALUE, CompareResult.UNSUPPORTED),//maxDelta + 1
+//			new Sample(-1, Long.MIN_VALUE, CompareResult.UNSUPPORTED),//maxDelta
+//			new Sample(Long.MAX_VALUE - Long.MAX_VALUE / 2, Long.MIN_VALUE + Long.MAX_VALUE / 2, CompareResult.UNSUPPORTED),//maxDelta
+//			new Sample(Long.MIN_VALUE, Long.MAX_VALUE, CompareResult.UNSUPPORTED),
+//			new Sample(Long.MIN_VALUE + 1000, Long.MAX_VALUE - 1000, CompareResult.UNSUPPORTED),
+//			new Sample(-2, 5, CompareResult.UNSUPPORTED),
+//			new Sample(5, 3, CompareResult.UNSUPPORTED),
+	};
+
 	public NanosComparatorTest() {
 	}
 
 	@Test
-	public final void compare() {
+	public final void compareUnsafe() {
 		for (final Sample sample : samples) {
-			assertCompare(sample.l1, sample.l2, sample.expected);
+			assertCompare(sample.l1, sample.l2, sample.expected, NanosComparator.getUnsafeInstance());
 		}
 	}
 
-	private static final void enshureCompatibility(final long l1, final long l2) {
-		long delta = l1 -l2;
-		assertTrue(String.format("l1 = %s, l2 = %s", l1, l2), delta != Long.MIN_VALUE);//Math.abs can't handle this value
-		assertTrue(String.format("l1 = %s, l2 = %s", l1, l2), Math.abs(delta) <= maxDelta);
+	@Test
+	public final void compare() {
+		for (final Sample sample : incompatibleSamples) {
+			assertCompare(sample.l1, sample.l2, sample.expected, NanosComparator.getInstance());
+		}
 	}
 
-	private static final void unsafeAssertCompareSymmetry(final long l1, final long l2) {
+	private static final void unsafeAssertCompareSymmetry(final Long l1, final Long l2, final NanosComparator comparator) {
 		assertSame(String.format("l1 = %s, l2 = %s", l1, l2),
-				Integer.signum(NanosComparator.compare(l1, l2)), -Integer.signum(NanosComparator.compare(l2, l1)));
+				Integer.signum(comparator.compare(l1, l2)), -Integer.signum(comparator.compare(l2, l1)));
 	}
 
-	private static final void assertCompare(final long l1, final long l2, final CompareResult expected) {
-		enshureCompatibility(l1, l2);
-		unsafeAssertCompareSymmetry(l1, l2);
+	private static final void assertCompare(final Long l1, final Long l2, final CompareResult expected, final NanosComparator comparator) {
+		if (expected != CompareResult.UNSUPPORTED) {
+			unsafeAssertCompareSymmetry(l1, l2, comparator);
+		}
 		switch (expected) {
 			case LOWER: {
-				assertTrue(String.format("l1 = %s, l2 = %s", l1, l2), NanosComparator.compare(l1, l2) < 0);
+				assertTrue(String.format("l1 = %s, l2 = %s", l1, l2), comparator.compare(l1, l2) < 0);
 				break;
 			}
 			case GREATER: {
-				assertTrue(String.format("l1 = %s, l2 = %s", l1, l2), NanosComparator.compare(l1, l2) > 0);
+				assertTrue(String.format("l1 = %s, l2 = %s", l1, l2), comparator.compare(l1, l2) > 0);
 				break;
 			}
 			case EQUAL: {
-				assertTrue(String.format("l1 = %s, l2 = %s", l1, l2), NanosComparator.compare(l1, l2) == 0);
+				assertTrue(String.format("l1 = %s, l2 = %s", l1, l2), comparator.compare(l1, l2) == 0);
+				break;
+			}
+			case UNSUPPORTED: {
+				boolean illegalArguments = false;
+				try {
+					comparator.compare(l1, l2);
+				} catch (final IllegalArgumentException e) {
+					illegalArguments = true;
+				}
+				assertTrue(String.format("l1 = %s, l2 = %s, illegalArguments = %s, comparator = %s", l1, l2, illegalArguments, comparator),
+						illegalArguments ^ (comparator == NanosComparator.getUnsafeInstance()));//use equals
 				break;
 			}
 			default: {
@@ -66,7 +91,8 @@ public final class NanosComparatorTest {
 	private enum CompareResult {
 		LOWER,
 		GREATER,
-		EQUAL;
+		EQUAL,
+		UNSUPPORTED;
 	}
 
 	private static final class Sample {
