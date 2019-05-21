eff7bda09ad20716c529c082b0f4a07bc2060142
Valentin Kovalenko <valentin.male.kovalenko@gmail.com>
2019-05-20 19:27:45 -0600

    Modify TmpTest to demonstrate potential JMH measurement problem

--- benchmarks/src/test/java/stincmale/sandbox/benchmarks/TmpTest.java
+++ benchmarks/src/test/java/stincmale/sandbox/benchmarks/TmpTest.java
@@ -1,11 +1,9 @@
 package stincmale.sandbox.benchmarks;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.concurrent.locks.StampedLock;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
@@ -13,181 +11,200 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.infra.BenchmarkParams;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import static org.openjdk.jmh.runner.options.TimeValue.milliseconds;
-import static stincmale.sandbox.benchmarks.util.JmhOptions.includeBenchmarks;
 
 /**
- * Test environment:
- * [single CPU] 3.4 GHz Intel Core i5 (4 cores, 4 hardware threads),
- * [OS] macOS 10.13.6 (17G4015),
- * [JDK] OpenJDK 11.0.1+13 (<a href="https://jdk.java.net/11/">a build from Oracle</a>).
- * <pre>{@code
- * 4 threads
- * Benchmark                 Mode  Cnt   Score    Error   Units
- * TmpTest.reentrantRwLock  thrpt    4  45.029 ± 35.699  ops/ms
- * TmpTest.stampedLock      thrpt    4  39.478 ± 55.836  ops/ms
+ * # JMH version: 1.21
+ * # VM version: JDK 12, OpenJDK 64-Bit Server VM, 12+33
+ * # VM invoker: /Library/Java/JavaVirtualMachines/jdk-12.jdk/Contents/Home/bin/java
+ * # VM options: -Xfuture --illegal-access=deny -Xms1024m -Xmx1024m
+ * # Warmup: <none>
+ * # Measurement: 2 iterations, 1000 ms each
+ * # Timeout: 1000000000 ms per iteration
+ * # Threads: 2 threads, will synchronize iterations
+ * # Benchmark mode: Throughput, ops/time
+ * # Benchmark: stincmale.sandbox.benchmarks.TmpTest.monitorSleep
  *
- * 32 threads
- * Benchmark                 Mode  Cnt     Score       Error   Units
- * TmpTest.reentrantRwLock  thrpt    4    35.167 ±    31.066  ops/ms
- * TmpTest.stampedLock      thrpt    4  2298.800 ± 19728.618  ops/ms
+ * # Run progress: 0.00% complete, ETA 00:00:06
+ * # Fork: 1 of 1
+ * Iteration   1:
+ * # non-JMH: throughput 0.997 ops/s (4 ops / 4.013 s)
+ * 1.330 ops/s
+ * Iteration   2:
+ * # non-JMH: throughput 0.999 ops/s (4 ops / 4.005 s)
+ * 0.999 ops/s
+ *
+ *
+ * Result "stincmale.sandbox.benchmarks.TmpTest.monitorSleep":
+ *   1.164 ops/s
  *
  *
  * # JMH version: 1.21
- * # VM version: JDK 11.0.2, OpenJDK 64-Bit Server VM, 11.0.2+9
- * # VM invoker: /Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java
- * # VM options: -Xfuture --illegal-access=deny -Xshare:off -Xms1024m -Xmx1024m -server -disableassertions
- * # Warmup: 6 iterations, 1000 ms each
+ * # VM version: JDK 12, OpenJDK 64-Bit Server VM, 12+33
+ * # VM invoker: /Library/Java/JavaVirtualMachines/jdk-12.jdk/Contents/Home/bin/java
+ * # VM options: -Xfuture --illegal-access=deny -Xms1024m -Xmx1024m
+ * # Warmup: <none>
  * # Measurement: 2 iterations, 1000 ms each
- * # Timeout: 1000000 ms per iteration
- * # Threads: 32 threads, will synchronize iterations
+ * # Timeout: 1000000000 ms per iteration
+ * # Threads: 2 threads, will synchronize iterations
  * # Benchmark mode: Throughput, ops/time
- * # Benchmark: stincmale.sandbox.benchmarks.TmpTest.stampedLock
+ * # Benchmark: stincmale.sandbox.benchmarks.TmpTest.sleep
  *
- * # Run progress: 50.00% complete, ETA 00:00:25
- * # Fork: 1 of 2
- * # Warmup Iteration   1: 14.844 ops/ms
- * # Warmup Iteration   2: 40.329 ops/ms
- * # Warmup Iteration   3: 44.688 ops/ms
- * # Warmup Iteration   4: 28.657 ops/ms
- * # Warmup Iteration   5: 18.564 ops/ms
- * # Warmup Iteration   6: 19.878 ops/ms
- * Iteration   1: 955.863 ops/ms
- * Iteration   2: 6793.449 ops/ms
+ * # Run progress: 33.33% complete, ETA 00:00:17
+ * # Fork: 1 of 1
+ * Iteration   1:
+ * # non-JMH: throughput 1.994 ops/s (4 ops / 2.006 s)
+ * 1.994 ops/s
+ * Iteration   2:
+ * # non-JMH: throughput 1.660 ops/s (5 ops / 3.012 s)
+ * 1.991 ops/s
  *
- * # Run progress: 75.00% complete, ETA 00:00:25
- * # Fork: 2 of 2
- * # Warmup Iteration   1: 3.991 ops/ms
- * # Warmup Iteration   2: 509.107 ops/ms
- * # Warmup Iteration   3: 26.467 ops/ms
- * # Warmup Iteration   4: 22.406 ops/ms
- * # Warmup Iteration   5: 33.943 ops/ms
- * # Warmup Iteration   6: 3147.069 ops/ms
- * Iteration   1: 19.063 ops/ms
- * Iteration   2: 1426.824 ops/ms
- * }</pre>
+ *
+ * Result "stincmale.sandbox.benchmarks.TmpTest.sleep":
+ *   1.992 ops/s
+ *
+ *
+ * # JMH version: 1.21
+ * # VM version: JDK 12, OpenJDK 64-Bit Server VM, 12+33
+ * # VM invoker: /Library/Java/JavaVirtualMachines/jdk-12.jdk/Contents/Home/bin/java
+ * # VM options: -Xfuture --illegal-access=deny -Xms1024m -Xmx1024m
+ * # Warmup: <none>
+ * # Measurement: 2 iterations, 1000 ms each
+ * # Timeout: 1000000000 ms per iteration
+ * # Threads: 2 threads, will synchronize iterations
+ * # Benchmark mode: Throughput, ops/time
+ * # Benchmark: stincmale.sandbox.benchmarks.TmpTest.writeLockSleep
+ *
+ * # Run progress: 66.67% complete, ETA 00:00:06
+ * # Fork: 1 of 1
+ * Iteration   1:
+ * # non-JMH: throughput 0.995 ops/s (4 ops / 4.020 s)
+ * 1.326 ops/s
+ * Iteration   2:
+ * # non-JMH: throughput 0.998 ops/s (13 ops / 13.031 s)
+ * 1.081 ops/s
+ *
+ *
+ * Result "stincmale.sandbox.benchmarks.TmpTest.writeLockSleep":
+ *   1.204 ops/s
+ *
+ *
+ * # Run complete. Total time: 00:00:31
+ *
+ * REMEMBER: The numbers below are just data. To gain reusable insights, you need to follow up on
+ * why the numbers are the way they are. Use profilers (see -prof, -lprof), design factorial
+ * experiments, perform baseline and negative tests that provide experimental control, make sure
+ * the benchmarking environment is safe on JVM/OS/HW level, ask for reviews from the domain experts.
+ * Do not assume the numbers tell you what you want them to tell.
+ *
+ * Benchmark                Mode  Cnt  Score   Error  Units
+ * TmpTest.monitorSleep    thrpt    2  1.164          ops/s
+ * TmpTest.sleep           thrpt    2  1.992          ops/s
+ * TmpTest.writeLockSleep  thrpt    2  1.204          ops/s
+ *
+ * Process finished with exit code 0
  */
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TmpTest {
   public TmpTest() {
   }
 
   public final void runThroughputBenchmarks(final int numberOfThreads) throws RunnerException {
     final OptionsBuilder opts = new OptionsBuilder();
-    opts.jvmArgs("-Xfuture", "--illegal-access=deny", "-Xshare:off", "-Xms1024m", "-Xmx1024m", "-server", "-disableassertions")
+    opts.jvmArgs("-Xfuture", "--illegal-access=deny", "-Xms1024m", "-Xmx1024m")
         .shouldDoGC(false)
         .syncIterations(true)
         .shouldFailOnError(true)
-        .threads(1)
-        .timeout(milliseconds(1000_000))
-        .forks(2)
+        .timeout(milliseconds(1000_000_000))
+        .forks(1)
         .warmupTime(milliseconds(1000))
         .warmupIterations(0)
         .measurementTime(milliseconds(1000))
-        .measurementIterations(5)
-        .include(includeBenchmarks(TmpTest.class))
-        .shouldDoGC(false)
+        .measurementIterations(2)
+        .include(getClass().getName() + ".*")
         .mode(Mode.Throughput)
         .timeUnit(TimeUnit.SECONDS)
         .threads(numberOfThreads);
     new Runner(opts.build()).run();
   }
 
-  @Test
-  public final void throughputThreads4() throws RunnerException {
-    runThroughputBenchmarks(4);
+  @Benchmark
+  public final void sleep(final BenchmarkState s) throws InterruptedException {
+    s.opsBeginCounter.increment();
+    {
+      Thread.sleep(1000);
+    }
+    s.opsEndCounter.increment();
   }
-
-  @Test
-  public final void throughputThreads32() throws RunnerException {
-    runThroughputBenchmarks(32);
-  }
-
-  //  @Benchmark
-  //  public final long reentrantRwLock(final BenchmarkState s) throws InterruptedException {
-  //    s.rwLock.readLock().stampedLock();
-  //    try {
-  //      if (s.counter.getAndIncrement() < 1000) {
-  //        return s.counter.get();
-  //      }
-  //    } finally {
-  //      s.rwLock.readLock().unlock();
-  //    }
-  //    s.rwLock.writeLock().stampedLock();
-  //    try {
-  //      Thread.sleep(10);
-  //      s.counter.set(0);
-  //    } finally {
-  //      s.rwLock.writeLock().unlock();
-  //    }
-  //    return s.counter.get();
-  //  }
 
   @Benchmark
-  public final long stampedLock(final BenchmarkState state) throws InterruptedException {
-    long counter;
-    boolean writeLock = false;
-    long stamp = state.stampedLock.readLock();
-    try {
-      for (; ; writeLock = true, stamp = state.stampedLock.writeLock()) {
-        counter = state.counter.incrementAndGet();
-        if (counter < 10) {//counter is fine
-          break;
-        } else {//counter is too big, we may need to reset counter
-          if (writeLock) {
-            //            System.out.println(String.format("tid=%s, counter=%s", Thread.currentThread().getId(), counter));
-            counter = 0;
-            state.counter.set(counter);
-            Thread.sleep(100);
-            break;
-          } else {
-            state.stampedLock.unlockRead(stamp);
-          }
-        }
-      }
-    } finally {
-      if (writeLock) {
-        state.stampedLock.unlockWrite(stamp);
-      } else {
-        state.stampedLock.unlockRead(stamp);
+  public final void monitorSleep(final BenchmarkState s) throws InterruptedException {
+    s.opsBeginCounter.increment();
+    {
+      synchronized (s.monitor) {
+        Thread.sleep(1000);
       }
     }
-    state.iterationCounter.getAndIncrement();
-    return counter;
+    s.opsEndCounter.increment();
+  }
+
+  @Benchmark
+  public final void writeLockSleep(final BenchmarkState s) throws InterruptedException {
+    s.opsBeginCounter.increment();
+    {
+      final ReentrantReadWriteLock.WriteLock wLock = s.rwLock.writeLock();
+      wLock.lock();
+      try {
+        Thread.sleep(1000);
+      } finally {
+        wLock.unlock();
+      }
+    }
+    s.opsEndCounter.increment();
   }
 
   @State(Scope.Benchmark)
   public static class BenchmarkState {
+    private Object monitor;
     private StampedLock stampedLock;
     private ReentrantReadWriteLock rwLock;
-    private AtomicLong counter;
-    private AtomicLong iterationCounter;
+    private LongAdder opsBeginCounter;
+    private LongAdder opsEndCounter;
+    private long startNanos;//JMH probably registers a later instant as the start time
 
     public BenchmarkState() {
     }
 
-    @Setup(Level.Trial)
+    @Setup(Level.Iteration)
     public final void setup() {
+      monitor = new Object();
       stampedLock = new StampedLock();
       rwLock = new ReentrantReadWriteLock();
-      counter = new AtomicLong();
-    }
-
-    @Setup(Level.Iteration)
-    public final void iterationSetUp() {
-      iterationCounter = new AtomicLong();
+      opsBeginCounter = new LongAdder();
+      opsEndCounter = new LongAdder();
+      startNanos = System.nanoTime();
     }
 
     @TearDown(Level.Iteration)
-    public final void iterationTearDown(final BenchmarkParams benchmarkParams) {
-      final TimeUnit timeUnit = benchmarkParams.getMeasurement().getTime().getTimeUnit();
-      final long measurementTime = benchmarkParams.getMeasurement().getTime().convertTo(benchmarkParams.getTimeUnit());
-      //      final int measurementIterations = benchmarkParams.getMeasurement().getCount();
-      System.out.println(String.format("actualCalculatedThroughput=%s ops/%s", iterationCounter.get() / measurementTime, timeUnit));
+    public final void tearDown() {
+      final long stopNanos = System.nanoTime();//JMH probably registers an earlier instant as the start time
+      final double durationSeconds = ((double)(stopNanos - startNanos)) / TimeUnit.SECONDS.toNanos(1);//JMH probably registers a smaller duration
+      final long opsBeginCount = opsBeginCounter.sum();
+      final double throughputBegin = opsBeginCount / durationSeconds;
+      final long opsEndCount = opsEndCounter.sum();
+      if (opsEndCount != opsBeginCount) {//should never happen, but I added just in case since the measurements are incorrect
+        throw new AssertionError(String.format("A benchmark method threw an exception that was swallowed by JMH %d times",
+            opsBeginCount - opsEndCount));
+      }
+      System.out.printf("%n# non-JMH: throughput %.3f ops/s (%d ops / %.3f s)%n", throughputBegin, opsBeginCount, durationSeconds);
     }
+  }
+
+  public static final void main(final String... args) throws RunnerException {
+    final int numberOfThreads = 2;
+    final TmpTest test = new TmpTest();
+    test.runThroughputBenchmarks(numberOfThreads);
   }
 }
